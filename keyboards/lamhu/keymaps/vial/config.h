bd7afde04b0599b7ba8fc4c235e5c08516d4c231
Lamhu956 <695912959@qq.com>
2025-03-14 08:48:46 +0800

    Create config.h

---
+++ keyboards/lamhu/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x89, 0x8F, 0x49, 0xEB, 0x5C, 0x21, 0xE3, 0xE9}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+/* VIAL secure unlock keystroke - currently both inner thumb keys (typ. SPACE/ENTER) */
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 2 }
